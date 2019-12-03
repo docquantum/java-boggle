c4de94aa309bb46694547f01a383ed2e5570ccba
Nirmitee <nirmitee.gite@huskers.unl.edu>
2019-12-03 16:45:30 -0600

    controller for Javafx with a new class to load the screens

--- src/main/java/edu/unl/cse/csce361/boggle/frontend/ScreenController.java
+++ src/main/java/edu/unl/cse/csce361/boggle/frontend/ScreenController.java
@@ -1,4 +1,85 @@
 package edu.unl.cse.csce361.boggle.frontend;
 
+import edu.unl.cse.csce361.boggle.logic.GameBoard;
+import javafx.application.Application;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ScreenController {
+
+    @FXML
+    private TextField playerInput;
+    @FXML
+    private ListView<String> wordViewer;
+    @FXML
+    private Label lbl0;
+    @FXML
+    private Label lbl1;
+    @FXML
+    private Label lbl2;
+    @FXML
+    private Label lbl3;
+    @FXML
+    private Label lbl4;
+    @FXML
+    private Label lbl5;
+    @FXML
+    private Label lbl6;
+    @FXML
+    private Label lbl7;
+    @FXML
+    private Label lbl8;
+    @FXML
+    private Label lbl9;
+    @FXML
+    private Label lbl10;
+    @FXML
+    private Label lbl11;
+    @FXML
+    private Label lbl12;
+    @FXML
+    private Label lbl13;
+    @FXML
+    private Label lbl14;
+    @FXML
+    private Label lbl15;
+
+
+
+    @FXML
+    public void singlePlay (Event event) throws IOException {
+        switchScreen(event, "BoggleScreen.fxml");
+        //implement switch screen to order summary here
+    }
+
+    @FXML
+    public void submitWord (Event event) throws IOException {
+        wordViewer.getItems().add(playerInput.getText());
+        playerInput.clear();
+        //implement switch screen to order summary here
+    }
+
+    public void switchScreen(Event event, String ScreenName) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(ScreenName));
+        Parent parent  =  loader.load();
+
+        Scene scene = new Scene(parent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(scene);
+        window.show();
+    }
+
 }
--- src/main/java/edu/unl/cse/csce361/boggle/logic/Solver.java
+++ src/main/java/edu/unl/cse/csce361/boggle/logic/Solver.java
@@ -10,11 +10,9 @@ public class Solver {
     public Integer boardWidth;
     public Integer boardHeight;
 
-    Solver(){
-
+    Solver() {
         boardWidth = 4;
         boardHeight = 4;
-
     }
 
     public void setDic(List<String> dic) {
@@ -25,68 +23,50 @@ public class Solver {
         return words;
     }
 
-    //    checks if sequence is in dictionary and not less than three letters
-    boolean isInDictionary(String sequence){
-
-        if(sequence.length() < 3){
-
+    //checks if sequence is in dictionary and not less than three letters
+    boolean isInDictionary(String sequence) {
+        if (sequence.length() < 3) {
             return false;
         }
-
-        for(int i = 0; i < this.dic.size(); i++){
-
-            if(sequence.equalsIgnoreCase(this.dic.get(i))){
+        for (int i = 0; i < this.dic.size(); i++) {
+            if (sequence.equalsIgnoreCase(this.dic.get(i))) {
                 return true;
             }
         }
         return false;
     }
 
-//    appends all valid sequences that start with char at index [i][j] on board to words arraylist
-    void localSequenceFinder(String board[][], boolean passed[][], int i, int j, String sequence){
-
-//        have already passed tree for this index
-        passed [i][j] = true;
+    //appends all valid sequences that start with char at index [i][j] on board to words arraylist
+    void localSequenceFinder(String board[][], boolean passed[][], int i, int j, String sequence) {
+        //have already passed tree for this index
+        passed[i][j] = true;
         sequence = sequence + board[i][j];
-
-//       append valid sequences that aren't duplicates to words arraylist
-        if(isInDictionary(sequence) && !this.words.contains(sequence)){
-
+        //append valid sequences that aren't duplicates to words arraylist
+        if (isInDictionary(sequence) && !this.words.contains(sequence)) {
             this.words.add(sequence);
         }
-
-//        navigating the adjacent cells relative to the specified char at index [i][j] (changes with each recursive call)
-        for(int row = i-1; row <= i+1 && row < this.boardHeight; row++){
-
-            for(int col = j-1; col <= j+1 && col < this.boardWidth; col++){
-
-                if(row >= 0 && col >= 0 && !passed[row][col]){
-
-//                    recursion for depth-first search of words starting with a specified char index within board
+        //navigating the adjacent cells relative to the specified char at index [i][j] (changes with each recursive call)
+        for (int row = i - 1; row <= i + 1 && row < this.boardHeight; row++) {
+            for (int col = j - 1; col <= j + 1 && col < this.boardWidth; col++) {
+                if (row >= 0 && col >= 0 && !passed[row][col]) {
+                    //recursion for depth-first search of words starting with a specified char index within board
                     localSequenceFinder(board, passed, row, col, sequence);
                 }
             }
         }
-
-//        resets as false so as to allow for another pass during another iteration of testing different permutations
+        //resets as false so as to allow for another pass during another iteration of testing different permutations
         passed[i][j] = false;
     }
 
-//    appends all valid sequences, accounting for all sequences with different starting letters, to words arraylist
+    //appends all valid sequences, accounting for all sequences with different starting letters, to words arraylist
     void entireSequenceFinder(String board[][]) {
-
-//        initialize passed board to null or false
+        //initialize passed board to null or false
         boolean passed[][] = new boolean[this.boardHeight][this.boardWidth];
-
+        //accounts for every letter on board as starting letter and then performs depth-first search
         String sequence = "";
-
-//        accounts for every letter on board as starting letter and then performs depth-first search
-        for(int i = 0; i < this.boardHeight; i++){
-
-            for(int j = 0; j < this.boardWidth; j++){
-
+        for (int i = 0; i < this.boardHeight; i++) {
+            for (int j = 0; j < this.boardWidth; j++) {
                 localSequenceFinder(board, passed, i, j, sequence);
-
             }
         }
     }
