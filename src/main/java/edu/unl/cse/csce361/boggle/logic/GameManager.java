8c0040192537a99aeebb97a96be455f6787185d6
jcantu <jcantu@huskers.unl.edu>
2019-12-03 22:56:07 +0000

    Added timer and new gameboard.

--- src/main/java/edu/unl/cse/csce361/boggle/logic/GameManager.java
+++ src/main/java/edu/unl/cse/csce361/boggle/logic/GameManager.java
@@ -3,6 +3,7 @@ package edu.unl.cse.csce361.boggle.logic;
 import java.awt.List;
 import java.util.ArrayList;
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class GameManager {
 
@@ -10,26 +11,41 @@ public class GameManager {
 	GameBoard board;
 	String gameBoard[][];
 	Timer time = new Timer();
+	TimerTask task = new TimerTask() {
+
+		@Override
+		public void run() {
+			
+			
+		}
+		
+	};
 	Solver getAnswers(String board[][]) {
 		answer.entireSequenceFinder(board);		
 		return answer;
 	}
-	
+	//gets the game board
 	String[][] getBoard() {
 		return board.getGameBoard();			
 	}
-	
+	//makes a new game-board
 	String[][] getNewBoard(){
 		board.generateGameBoard();
 		return board.getGameBoard();
 	}
-	
+	//gets the score for singlePlayer
 	int getScores(List singlePlayer1){
 		
-		List singlePlayer = singlePlayer1;
+		
+		int scores = 0;
 		
 		
 		
+		
+		return scores;
+	}
+	//Save for Sprint 2
+	int getScores(ArrayList<ArrayList<String>> Players){
 		
 		int scores = 0;
 		
@@ -37,12 +53,11 @@ public class GameManager {
 		return scores;
 	}
 	
-	
-	
+	//gets timer
 	Timer getTimer() {
 		return time;
 	}
-	
+	//Sets a new timer
 	Timer resetTimer() {
 		time = new Timer();
 		return time;
