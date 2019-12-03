e2fe4d3dcb0604659640616f6a927891516f36c2
Nirmitee <nirmitee.gite@huskers.unl.edu>
2019-12-03 16:58:09 -0600

    Added another fxml screen for when game ends

--- src/main/java/edu/unl/cse/csce361/boggle/logic/GameManager.java
+++ src/main/java/edu/unl/cse/csce361/boggle/logic/GameManager.java
@@ -6,46 +6,39 @@ import java.util.Timer;
 
 public class GameManager {
 
-	Solver answer;
-	GameBoard board;
-	String gameBoard[][];
-	Timer time = new Timer();
-	Solver getAnswers(String board[][]) {
-		answer.entireSequenceFinder(board);		
-		return answer;
-	}
-	
-	String[][] getBoard() {
-		return board.getGameBoard();			
-	}
-	
-	String[][] getNewBoard(){
-		board.generateGameBoard();
-		return board.getGameBoard();
-	}
-	
-	int getScores(List singlePlayer1){
-		
-		List singlePlayer = singlePlayer1;
-		
-		
-		
-		
-		int scores = 0;
-		
-		
-		return scores;
-	}
-	
-	
-	
-	Timer getTimer() {
-		return time;
-	}
-	
-	Timer resetTimer() {
-		time = new Timer();
-		return time;
-	}
-	
+    Solver answer;
+    GameBoard board;
+    String gameBoard[][];
+    Timer time = new Timer();
+
+    Solver getAnswers(String board[][]) {
+        answer.entireSequenceFinder(board);
+        return answer;
+    }
+
+    String[][] getBoard() {
+        return board.getGameBoard();
+    }
+
+    String[][] getNewBoard() {
+        board.generateGameBoard();
+        return board.getGameBoard();
+    }
+
+    int getScores(List singlePlayer1) {
+        List singlePlayer = singlePlayer1;
+        int scores = 0;
+        return scores;
+    }
+
+
+    Timer getTimer() {
+        return time;
+    }
+
+    Timer resetTimer() {
+        time = new Timer();
+        return time;
+    }
+
 }
