9573bbeaf1ed0854aedec173343830d6c20f4827
DocQuantum <shchurgood@gmail.com>
2019-12-08 15:57:32 -0600

    Implemented networking... Needs a lot of work

---
+++ src/main/java/edu/unl/cse/csce361/boggle/backend/network/DataCodes.java
@@ -0,0 +1,15 @@
+package edu.unl.cse.csce361.boggle.backend.network;
+
+/**
+ * List of codes that denote what is being requested and sent
+ * over the network.
+ */
+public enum DataCodes {
+    PLAYER_NAME,
+    ALL_PLAYERS,
+    GAME_BOARD,
+    WORD_LIST,
+    ALL_SCORES,
+    SCORE,
+    EXIT
+}
